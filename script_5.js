dd458002c924871b247a4f9212ba3afc67d704ce
AlexbyID <noragamiset1000@gmail.com>
2022-05-28 01:46:27 +0400

    ce

--- script_5.js
+++ script_5.js
@@ -1,21 +1,41 @@
 
-let screenHeight = window.screen.height;
-let screenWidth = window.screen.width;
-let root = document.querySelector("html");
-var get = document.querySelector(".main-sticky-frame-padding");
-console.log(get.clientHeight);
-function scrollEvent(elem) {
-  var delta = elem.deltaY;
-  if(delta>0){
-    console.log("1");
-    get.scrollTo(50,50);
-  }
-}
-root.addEventListener("wheel", scrollEvent);
-
-
 function scrollNachalo() {
   if(history.scrollRestoration)
     history.scrollRestoration === 'manual';
 }
 window.addEventListener("beforeunload", scrollNachalo);
+
+
+let scrWidth = window.screen.width;
+let delVd = document.querySelector(".header-left");
+let vidjet = document.querySelector(".header-inner");
+let fxpos, icVidjet;
+
+if(scrWidth<1570){
+  vidjet.removeChild(delVd);
+  var vd = document.createElement("div");
+  var vd_2 = document.createElement("div");
+  var vd_3 = document.createElement("div");
+  vidjet.prepend(vd);
+  vd.classList.add("vidjet-left-screen");
+  vd.prepend(vd_2);
+  vd_2.classList.add("vidjet-white-back");
+  var ic = document.createElement("i");
+  vd.prepend(vd_3);
+  vd_3.classList.add("fix-pos-ic-vidjet");
+  vd_3.prepend(ic);
+  ic.classList.add("ic-vidjet");
+  fxpos = document.querySelector(".fix-pos-ic-vidjet");
+  fxpos.style = "width: inherit; height: 137px; display: flex; justify-content: center; align-items: center;background-color: #fff;";
+}
+if(scrWidth<1165)
+  fxpos.style.backgroundColor = "#F5F5FB";
+
+if(scrWidth<800){
+  fxpos.style.cssText = "";
+  icVidjet = document.querySelector(".ic-vidjet");
+  icVidjet.style = "width: 40px; height: 40px;";
+}
+
+if(scrWidth<348)
+  icVidjet.style.cssText = "width: 30px; height: 30px;";
